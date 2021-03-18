8bdfd385382898ffda64c17566e1d81a25030540
Rajeev Ravindran <rajeev.ravindran@medimpact.com>
2021-03-17 17:25:25 -0700

    adding kafka listeners as Functions

---
+++ src/main/java/com/example/study/kafka/demo/consumer/NdcProcessConsumer.java
@@ -0,0 +1,23 @@
+package com.example.study.kafka.demo.consumer;
+
+import com.example.study.kafka.demo.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+@Profile(value = "!test")
+@Slf4j
+public class NdcProcessConsumer {
+
+
+    @Bean
+    public Consumer<KStream<?, User>> processNdc() {
+        return processNdcStream ->
+                processNdcStream.foreach((k,v)-> log.info("{} value: {}", k , v));
+    }
+}
